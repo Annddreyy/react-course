47b7e36d290472d42c3257f8f446efdbc572e931
Andrey22 <andreymail22112006@gmail.com>
2025-04-07 18:02:20 +0300

    fix: update browser navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,30 @@ import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 
 
 const App = (props) => {
 	return (
-		<BrowserRouter>
-			<div className="App">
-				<Header />
-				<Navbar state={props.appState.sidebar} />
-				<main className="main">
-					<Routes>
-						<Route path="/dialogs/*" element={
-							<Dialogs 
-							state={props.appState.messagesPage} 
-							/>
-						} />
-						<Route path="/profile" element={
-							<Profile 
-							state={props.appState.profilePage} 
-							/>
-						} />
-					</Routes>
-				</main>
-			</div>
-		</BrowserRouter>
-	);
+		<div className="App">
+			<Header />
+			<Navbar state={props.appState.sidebar} />
+			<main className="main">
+				<Routes>
+					<Route path="/dialogs/*" element={
+						<Dialogs 
+						state={props.appState.messagesPage} 
+						/>
+					} />
+					<Route path="/profile" element={
+						<Profile 
+						state={props.appState.profilePage} 
+						/>
+					} />
+				</Routes>
+			</main>
+		</div>
+	)
 };
 
 export default App;
