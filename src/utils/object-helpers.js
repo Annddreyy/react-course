ad4129c90d77710c481b2267c3c4d6d91f8a8f1b
Annddreyy <109622384+Annddreyy@users.noreply.github.com>
2025-04-30 22:53:27 +0300

    Delete src directory

--- src/utils/object-helpers.js
+++
@@ -1,8 +0,0 @@
-export let updateObjectInArray = (items, checkValue, objectPropName, newObjProps) => {
-    return items.map(item => {
-        if (item[objectPropName] === checkValue) {
-            return { ...item, ...newObjProps }
-        }
-        return item;
-    });
-};
