27feb3412dffcc9ae0043a0034d9083ae06348cd
Andrey22 <andreymail22112006@gmail.com>
2025-05-08 21:31:19 +0300

    feat: add types to hoc

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -1,18 +1,31 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Navigate } from "react-router-dom";
+import {connect} from "react-redux";
+import {AppStateType} from '../redux/redux-store';
+
+let mapStateToPropsForRedirect = (state: AppStateType) => ({
+    isAuth: state.auth.isAuth
+} as MapPropsType);
 
 type MapPropsType = {
     isAuth: boolean
-};
+}
+type DispatchPropsType = {
+}
 
-export const withAuthRedirect = <WCP>(Component: React.ComponentType<WCP>) => {
-    return (props: WCP & MapPropsType) => {
-        let { isAuth, ...restProps } = props;
-        if (!isAuth) {
-            return <Navigate to="/login" />;
-        }
-        return <Component { ...restProps } />;
+export function withAuthRedirect<WCP extends React.JSX.IntrinsicAttributes>(WrappedComponent: React.ComponentType<WCP>) {
+
+    const RedirectComponent: React.FC<MapPropsType & DispatchPropsType> = (props) => {
+        let {isAuth, ...restProps} = props
+
+        if (!isAuth) return <Navigate to='/login'/>
+
+        return <WrappedComponent {...restProps as WCP}/>
     }
-};
 
+    let ConnectedAuthRedirectComponent = connect<MapPropsType, DispatchPropsType, WCP, AppStateType>(
+        mapStateToPropsForRedirect, {})
+    (RedirectComponent)
+
+    return ConnectedAuthRedirectComponent;
+}
